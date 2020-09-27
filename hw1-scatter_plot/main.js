795e56e00a73c761ac1248fcd1aebf2e06764f8a
cysun0226 <j6256448@gmail.com>
2020-09-27 17:39:26 +0800

    feat: add dropdown selections for x_axis and y_axis, fix the scatter plot bug

--- hw1-scatter_plot/main.js
+++ hw1-scatter_plot/main.js
@@ -1,11 +1,17 @@
 /*
     reference: https://www.d3-graph-gallery.com/graph/scatter_grouped.html
+    d3 select: https://codepen.io/tarsusi/pen/reovOV
 */
+
+const MAX_WIDTH = 800;
+const MAX_HEIGHT = 800;
+const BACKGROUND = "#F8F9FA";
+const PALETTE = ["#a2d6f9", "#f4d35e", "#28afb0"];
 
 // set the dimensions and margins of the graph
 var margin = { top: 10, right: 30, bottom: 30, left: 60 },
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    width = MAX_WIDTH - margin.left - margin.right,
+    height = MAX_HEIGHT - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#scatter_plot")
@@ -26,9 +32,50 @@ d3.csv("data/iris.csv", data => {
         class: data["class"]
     };
 }).then(data => {
+    // attributes
+    let allGroup = ["sepal_length", "sepal_width", "petal_length", "petal_width"];
+
+    // add selection options
+    d3.select("#select_x")
+        .selectAll("option")
+        .data(allGroup)
+        .enter()
+        .append('option')
+        .text(d => { return d; }) // the text showed in the menu
+        .attr("value", d => { return d; }); // selected option
+
+    d3.select("#select_y")
+        .selectAll("option")
+        .data(allGroup)
+        .enter()
+        .append('option')
+        .text(d => { return d; })
+        .attr("value", d => { return d; });
+
+    // default value and set the selector
+    let x_option = "",
+        y_option = "";
+    d3.select("#select_x").property("selectedIndex", 0);
+    d3.select("#select_y").property("selectedIndex", 1);
+
+    // when the user selects an option, update
+    d3.select("#select_x")
+        .on("change", d => {
+            x_option = d3.select("#select_x").node().value;
+            console.log("x = " + x_option);
+        })
+
+    d3.select("#select_y")
+        .on("change", d => {
+            y_option = d3.select("#select_y").node().value;
+            console.log("y = " + y_option);
+        })
+
+    // Update the plot
+
     // X axis
     let x = d3.scaleLinear()
-        .domain([0, 10])
+        .domain([4, 8])
         .range([0, width]);
     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
@@ -44,7 +91,7 @@ d3.csv("data/iris.csv", data => {
     // Color setting: plot three classes with different colors
     let color = d3.scaleOrdinal()
         .domain(["Iris-setosa", "Iris-versicolor", "Iris-virginica"])
-        .range(["#a2d6f9", "#f4d35e", "#28afb0"])
+        .range(["#a2d6f9", "#f4d35e", "#28afb0"]);
 
     // Plot the data
     svg.append("g")
@@ -53,9 +100,9 @@ d3.csv("data/iris.csv", data => {
         .enter()
         .append("circle")
         .attr("cx", d => { return x(d.sepal_length); })
-        .attr("cy", d => { return x(d.petal_length); })
+        .attr("cy", d => { return y(d.petal_length); })
         .attr("r", 5)
-        .style("fill", d => { return color(d.class) })
+        .style("fill", d => { return color(d.class) });
 });
 
 // console.log(iris_data);
