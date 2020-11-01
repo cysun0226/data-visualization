74eb1d39bcb9af22bd461952c003ef41c8678a1c
Chia-Yu Sun <chiayusun.cs08g@nctu.edu.tw>
2020-11-02 00:14:41 +0800

    feat(hw3): add network plot example

--- hw3-graph/0856044.js
+++ hw3-graph/0856044.js
@@ -4,6 +4,8 @@ var adjacency_matrix;
 const NODE_NUM = 410;
 const ADJ_MATRIX_WIDTH = 1000;
 const ADJ_MATRIX_HEIGHT = 1000;
+const LINK_NETWORK_WIDTH = 1000;
+const LINK_NETWORK_HEIGHT = 1000;
 const ADJ_BLOCK_SIZE = 2;
 
 
@@ -78,7 +80,8 @@ function createAdjacencyMatrix(nodes, edges) {
         .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
 
-    d3.select("svg").append("g")
+    svg.selectAll("adj-block")
+        .append("g")
         .attr("transform", "translate(50,50)")
         .attr("id", "adjacencyG")
         .selectAll("rect")
@@ -124,3 +127,119 @@ function createAdjacencyMatrix(nodes, edges) {
     };
 
 };
+
+function createNetworkGraph(){
+    var margin = {
+            top: 10,
+            right: 10,
+            bottom: 10,
+            left: 10
+        },
+    width = LINK_NETWORK_WIDTH - margin.left - margin.right,
+    height = LINK_NETWORK_HEIGHT - margin.top - margin.bottom;
+
+    var svg = d3.select("#node-link_div")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+    var simulation = d3.forceSimulation()
+        .force("link", d3.forceLink().id(function (d) {
+            return d.id;
+        }))
+        .force("charge", d3.forceManyBody())
+        .force("center", d3.forceCenter(width / 2, height / 2));
+
+    d3.json("data/miserables.json", function (error, graph) {
+        if (error) throw error;
+
+        var link = svg.append("g")
+            .attr("class", "links")
+            .selectAll("line")
+            .data(graph.links)
+            .enter().append("line")
+            .attr("stroke-width", function (d) {
+                return Math.sqrt(d.value);
+            });
+
+        var node = svg.append("g")
+            .attr("class", "nodes")
+            .selectAll("g")
+            .data(graph.nodes)
+            .enter().append("g")
+
+        var circles = node.append("circle")
+            .attr("r", 5)
+            .attr("fill", function (d) {
+                return color(d.group);
+            })
+            .call(d3.drag()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended));
+
+        var lables = node.append("text")
+            .text(function (d) {
+                return d.id;
+            })
+            .attr('x', 6)
+            .attr('y', 3);
+
+        node.append("title")
+            .text(function (d) {
+                return d.id;
+            });
+
+        simulation
+            .nodes(graph.nodes)
+            .on("tick", ticked);
+
+        simulation.force("link")
+            .links(graph.links);
+
+        function ticked() {
+            link
+                .attr("x1", function (d) {
+                    return d.source.x;
+                })
+                .attr("y1", function (d) {
+                    return d.source.y;
+                })
+                .attr("x2", function (d) {
+                    return d.target.x;
+                })
+                .attr("y2", function (d) {
+                    return d.target.y;
+                });
+
+            node
+                .attr("transform", function (d) {
+                    return "translate(" + d.x + "," + d.y + ")";
+                })
+        }
+    });
+
+    function dragstarted(d) {
+        if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+    }
+
+    function dragged(d) {
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+    }
+
+    function dragended(d) {
+        if (!d3.event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
+    }
+}
+
+// createNetworkGraph();
